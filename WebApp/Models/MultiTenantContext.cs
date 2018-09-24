54d7ca2ef62bd3ac8a6547507f15a936d73eb708
Arunkumar Ramachandriya <cloud.fortytwo@gmail.com>
2018-09-24 16:42:05 +0100

    Added EF

---
+++ WebApp/Models/MultiTenantContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+
+namespace WebApp.Models
+{
+    public class MultiTenantContext : DbContext
+    {
+        public MultiTenantContext()
+        {
+            this.Database.Connection.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=WebApp.Models.MultiTenantContext;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        }
+        public DbSet<Tenant> Tenants { get; set; }
+        public DbSet<Speaker> Speakers { get; set; }
+        public DbSet<Session> Sessions { get; set; }
+    }
+}
