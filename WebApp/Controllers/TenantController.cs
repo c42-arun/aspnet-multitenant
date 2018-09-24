0179597e1eb82a0a41c272fe0b4465d0317fba04
Arunkumar Ramachandriya <cloud.fortytwo@gmail.com>
2018-09-24 16:01:02 +0100

    Tenant Controller/View/Model added

---
+++ WebApp/Controllers/TenantController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class TenantController : Controller
+    {
+        public ActionResult Index()
+        {
+            var tenants = new List<Tenant>
+            {
+                new Tenant
+                {
+                    Name = "SVCC",
+                    DomainName = "www.siliconvalley-codecamp.com",
+                    Id = 1,
+                    Default = true
+                },
+                new Tenant()
+                {
+                    Name = "ANGU",
+                    DomainName = "angularu.com",
+                    Id = 3,
+                    Default = false
+                },
+                new Tenant()
+                {
+                    Name = "CSSC",
+                    DomainName = "codestarssummit.com",
+                    Id = 2,
+                    Default = false
+                }
+
+            };
+
+            return View(tenants);
+        }
+    }
+}
