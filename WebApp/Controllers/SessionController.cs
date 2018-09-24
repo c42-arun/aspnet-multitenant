125f62fd35d0c0fcce70bbc31188b9006ab1b00f
Arunkumar Ramachandriya <cloud.fortytwo@gmail.com>
2018-09-24 16:07:05 +0100

    Added Speaker/Session controller/model scaffolding

---
+++ WebApp/Controllers/SessionController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class SessionController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApp/Controllers/SpeakerController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class SpeakerController : Controller
+    {
+        public ActionResult Index()
+        {
+             return View();
+        }
+    }
+}
