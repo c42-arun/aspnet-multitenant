5dd622dc21c490b9c01ae9dbe8bc5ca8543ea8a9
Arunkumar Ramachandriya <cloud.fortytwo@gmail.com>
2018-09-25 10:26:36 +0100

    Remaining overridable members in custom view engine implemented

--- WebApp/MultiTenantViewEngine.cs
+++ WebApp/MultiTenantViewEngine.cs
@@ -19,6 +19,19 @@ namespace WebApp
                 "~/Views/%1/Shared/{0}.cshtml",
                 "~/Views/Shared/{0}.cshtml"
             };
+
+            PartialViewLocationFormats = ViewLocationFormats;
+            MasterLocationFormats = ViewLocationFormats;
+
+            AreaMasterLocationFormats = new[]
+            {
+                "~/Areas/%1/Views/{1}/{0}.cshtml",
+                "~/Areas/%1/Views/{1}/{0}.vbhtml",
+                "~/Areas/%1/Views/Shared/{0}.cshtml",
+                "~/Areas/%1/Views/Shared/{0}.vbhtml",
+            };
+
+            AreaPartialViewLocationFormats = AreaMasterLocationFormats;
         }
 
         protected override IView CreateView(ControllerContext controllerContext, string viewPath, string masterPath)
@@ -30,6 +43,15 @@ namespace WebApp
             return base.CreateView(controllerContext,
                 viewPath.Replace("%1", controller.Tenant.Name),
                 masterPath.Replace("%1", controller.Tenant.Name));
+        }
+
+        protected override IView CreatePartialView(ControllerContext controllerContext, string partialPath)
+        {
+            var controller = controllerContext.Controller as MultiTenantMvcController;
+
+            Debug.Assert(controller != null, "PassedController != null");
+
+            return base.CreatePartialView(controllerContext, partialPath.Replace("%1", controller.Tenant.Name);
         }
 
         protected override bool FileExists(ControllerContext controllerContext, string virtualPath)
