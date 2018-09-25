bf1d9d6784f43eb0b67d8848b51643c0b4fde0b3
Arunkumar Ramachandriya <cloud.fortytwo@gmail.com>
2018-09-25 10:53:37 +0100

    Added sessions to speakers in view; async view

--- WebApp/Controllers/SpeakerController.cs
+++ WebApp/Controllers/SpeakerController.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web.Mvc;
 using WebApp.Models;
 
@@ -7,16 +9,37 @@ namespace WebApp.Controllers
 {
     public class SpeakerController : MultiTenantMvcController
     {
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
             var speakers = new List<Speaker>();
-
+            
             using (var context = new MultiTenantContext())
             {
-                speakers = (from speaker in context.Speakers
-                            let sessionInTenant = speaker.Sessions.Any(a => a.Tenant.Name == Tenant.Name)
-                            where sessionInTenant
-                            select speaker).ToList();
+                var speakersAll = await context.Speakers.ToListAsync();
+
+                foreach (var speaker in speakersAll)
+                {
+                    bool speakerInTenant = speaker.Sessions.Any(a => a.Tenant.Name == Tenant.Name);
+
+                    if (speakerInTenant)
+                    {
+                        speakers.Add(new Speaker
+                        {
+                            FirstName = speaker.FirstName,
+                            LastName = speaker.LastName,
+                            Id = speaker.Id,
+                            Bio = speaker.Bio,
+                            Website = speaker.Website,
+                            AllowHtml = speaker.AllowHtml,
+                            ImageUrl = speaker.ImageUrl,
+                            PictureId = speaker.PictureId,
+                            Sessions =
+                                speaker.Sessions
+                                    .Where(a => a.Tenant.Name == Tenant.Name)
+                                    .OrderBy(a => a.Title).ToList()
+                        });
+                    }
+                }
             }
 
             return View("Index", speakers);
