cddc0828a4f1789b43628e4e1ebbf989307b4bce
Arunkumar Ramachandriya <cloud.fortytwo@gmail.com>
2018-09-25 08:42:08 +0100

    Added OWIN Startup to get tenant added to context

---
+++ WebApp/Startup.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebApp.Models;
+
+[assembly: OwinStartup(typeof(WebApp.Startup))]
+
+namespace WebApp
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Use(async (ctx, next) =>
+            {
+                Tenant tenant = GetTenantBasedOnUrl(ctx.Request.Uri.Host);
+                if (tenant == null)
+                {
+                    throw new ApplicationException("tenant not found");
+                }
+
+                ctx.Environment.Add("MultiTenant", tenant);
+                await next();
+            });
+        }
+
+        private Tenant GetTenantBasedOnUrl(string urlhost)
+        {
+            if (string.IsNullOrEmpty(urlhost))
+            {
+                throw new ApplicationException("urlHost must be specified");
+            }
+
+            Tenant tenant;
+            using (var context = new MultiTenantContext())
+            {
+                DbSet<Tenant> tenants = context.Tenants;
+
+                tenant = tenants.FirstOrDefault(a => a.DomainName.ToLower().Equals(urlhost))
+                            ?? tenants.FirstOrDefault(a => a.Default);                
+            }
+
+            return tenant;
+        }
+    }
+}
