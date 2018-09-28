825885a9deb73d7bda463b1b9169645143cbd1ad
Arunkumar Ramachandriya <cloud.fortytwo@gmail.com>
2018-09-28 10:23:44 +0100

    Added watchify to gulp task to watch for 'main.js' changes and auto refresh browserSync

--- WebApp/ng/gulpfile.js
+++ WebApp/ng/gulpfile.js
@@ -3,15 +3,30 @@ var gutil = require('gulp-util');
 var browserSync = require('browser-sync').create();
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var watchify = require('watchify');
 
 gulp.task('scripts', function() {
-    browserify('./svcc/main.js')
-    .bundle()
-    .on('error', function(e) {
-        gutil.log(e);
-    })
-    .pipe(source('app.js'))
-    .pipe(gulp.dest('./dist'));
+    bundleAndBrowse(browserify('./svcc/main.js'));
+});
+
+gulp.task('watch', function() {
+    var watcher = watchify(browserify('./svcc/main.js', watchify.args));
+
+    // first run
+    bundleAndBrowse(watcher); 
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        }
+    });    
+
+    // subsequent runs on file change
+    watcher.on('update', function() {
+        bundleAndBrowse(watcher);
+    });
+
+    watcher.on('update', gutil.log);
+
 });
 
 gulp.task('default', function() {
@@ -21,4 +36,15 @@ gulp.task('default', function() {
         }
     });
 });
+
+function bundleAndBrowse(bundler) {
+    return bundler
+        .bundle()
+        .on('error', function(e) {
+            gutil.log(e);
+        })
+        .pipe(source('app.js'))
+        .pipe(gulp.dest('./dist'))
+        .pipe(browserSync.stream()); // automatically push to browserify
+}
 
