ff39b7d5397a3b8da14daf6347839d4c331055c6
Arunkumar Ramachandriya <cloud.fortytwo@gmail.com>
2018-10-02 16:35:42 +0100

    added angular as "requires" and see if angular is correcty loaded in browser by avaluating an expression

--- WebApp/ng/svcc/main.js
+++ WebApp/ng/svcc/main.js
@@ -1,5 +1,1 @@
-var calcAge = require('./calcAge.js');
-
-document.getElementsByTagName('body')[0].onload = function() {
-    document.body.innerHTML = 'The answer is:' + calcAge.gatesAge();
-};
+require('angular');
